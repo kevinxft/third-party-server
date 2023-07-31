b5d31af82a707bb7cc7efc9a91e66454f003d0b9
MR.Kevin <kevinxft@gmail.com>
2023-07-31 13:20:37 +0800

    优化历史消息，提高成功率

--- apps/all-in-one/src/azure/utils/index.ts
+++ apps/all-in-one/src/azure/utils/index.ts
@@ -1,6 +1,12 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
+const maxhistory = 5;
+const historyMessages = [];
 
-export const initMessages = (words: string[]) => [
+type messageType = {
+  role: string;
+  content: string;
+};
+
+const preMessages: messageType[] = [
   {
     role: 'user',
     content:
@@ -10,32 +16,53 @@ export const initMessages = (words: string[]) => [
     role: 'user',
     content: `如果有时态变化,要存一份单词的时态在tense里,返回的格式严格按照这个JSON模板 {"sentence":"","trans": "","tense":{"read":"reading","watch":"watched"},"positoins":{},"tips":""}`,
   },
-  {
-    role: 'user',
-    content: `帮我用这${words.length}个单词${words.join(
-      ',',
-    )}造一个英文句子放到JSON的sentence,句子的翻译放到JSON的trans,单词在句子中的位置放到JSON的positions,单词在句子中变化后的时态放到JSON的tense,没有的不放,如果你有什么补充,以中文放到JSON的tips`,
-  },
 ];
+
+export const initUserMessage = (words: string[]) => ({
+  role: 'user',
+  content: `帮我用这${words.length}个单词${words.join(
+    ',',
+  )}造一个英文句子放到JSON的sentence,句子的翻译放到JSON的trans,单词在句子中的位置放到JSON的positions,单词在句子中变化后的时态放到JSON的tense,没有的不放,如果你有什么补充,以中文放到JSON的tips`,
+});
+
+const addHistoryMessage = (message) => {
+  if (historyMessages.length >= maxhistory) {
+    historyMessages.shift();
+  }
+  historyMessages.push({
+    role: 'assistant',
+    content: JSON.stringify(message),
+  });
+};
+
+export const initMessages = (words) => {
+  let messages: messageType[] = [...preMessages];
+  messages = [...messages, ...historyMessages];
+  messages.push(initUserMessage(words));
+  return messages;
+};
 
 export const toJson = (str) => {
   try {
-    return JSON.parse(str);
+    const res = JSON.parse(str);
+    addHistoryMessage(res);
+    return res;
   } catch (error) {
     console.log('不是json，尝试转换');
     str.replaceAll('，', ',');
     str = str.trim();
-    let start = 0;
-    let end = str.length;
-    while (str[start++] !== '{') {}
-    while (str[end--] !== '}') {}
-    const cutStr = str.slice(start - 1, end + 2);
+    const start = str.indexOf('{');
+    const end = str.lastIndexOf('}') + 1;
+    const cutStr = str.slice(start, end);
     console.log('cut after: ', cutStr);
     try {
-      return JSON.parse(str.slice(start - 1, end + 2));
+      return JSON.parse(str.slice(start, end));
     } catch (error) {
       console.log(error);
-      throw new HttpException('openai返回格式出错', HttpStatus.BAD_REQUEST);
+      return {
+        statusCode: 250,
+        message: 'openai返回格式出错',
+      };
     }
   }
 };
